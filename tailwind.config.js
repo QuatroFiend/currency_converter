75edf298e243db9f47fac27ef2add658582f5209
QuatroFiend <buddahvamp@gmail.com>
2025-07-19 13:20:32 +0200

    Adding responsive design
    Fixed build , reinstalled dependencies , changed version of tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
-
+/** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-        './app/**/*.{js,ts,jsx,tsx}',
-        './src/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-        screens: {
-            'xs': '380px',
-            'sm': '640px',
-            'md': '860px',
-            'lg': '1280px',
-        },
-    },
-    plugins: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
+
